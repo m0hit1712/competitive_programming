8fb3d2792ef6d56a02b485c624ac5e4dae28ee75
m0hit1712 <mohit.patidar1503@gmail.com>
2021-02-13 23:11:02 +0530

    linked list c++ header added and python linked list intialized

--- data_structures/python/singly_linked_list.py
+++ data_structures/python/singly_linked_list.py
@@ -1,6 +1,31 @@
+import sys
+def stdwrite(line): return sys.stdout.write(line+"\n")
 
 
+class node:
+        data = 0
+        next = 0
+        def insert_at_beg(self):
+                pass
+        def insert_at_end(self):
+                pass
+        def insert_at_n(self):
+                pass
+        def delete_from_beg(self):
+                pass
+        def delete_from_end(self):
+                pass
+        def delete_from_n(self):
+                pass
+        def display(self):
+                pass
+        def custom_display(self):
+                pass
+        def reverse(self):
+                pass
 
+if __name__ == '__main__':
+        pass
 
 
 
