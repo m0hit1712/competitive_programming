5a486b4b8d1fc997792d3f33f0dc7ac6cc908700
m0hit1712 <mohit.patidar1503@gmail.com>
2021-02-13 07:12:22 +0530

    general

---
+++ data_structures/c++/singly_linked_list.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+        int x;
+        node *next;
+};
+
+int main()
+{
+
+}
+
+
+
+
---
+++ mathematical_problems/python_programs/prime_numbers.py
@@ -0,0 +1,5 @@
+import sys
+stdint =lambda: int(sys.stdin.readline())
+stdwrite =lambda line: sys.stdout.write(line+"\n")  
+
+
